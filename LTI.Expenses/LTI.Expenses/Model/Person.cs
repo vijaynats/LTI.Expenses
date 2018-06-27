a51fdc84396168ddd9555b5166212fb6ee47f508
Varun <varunsaravanan31@gmail.com>
2018-06-26 23:30:55 -0700

    Class created for button click

---
+++ LTI.Expenses/LTI.Expenses/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTI.Expenses.Model
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public string DisplayName {
+            get; set;
+        }
+
+        public string Email {
+            get; set;
+        }
+
+        public string LoginName
+        {
+            get; set;
+        }
+    }
+}
